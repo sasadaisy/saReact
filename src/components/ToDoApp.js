5d4271622157ae528fd9e8247a368bc3eb7cad1a
sasadaisy <daisysasa@163.com>
2017-09-23 14:45:38 +0800

    xs
    react exercise

---
+++ src/components/ToDoApp.js
@@ -0,0 +1,24 @@
+/**
+ * Created by mac on 2017/9/23.
+ */
+
+class ToDoApp extends React.Component {
+    render() {
+        return (
+            <div className="row">
+            <div className="col-md-10 col-md-offset-1">
+            <div className="panel panel-default">
+            <div className="panel-body">
+            <h1>My To Do App</h1>
+        <hr/>
+        List goes here.
+        </div>
+        </div>
+        </div>
+        </div>
+    );
+    }
+}
+
+
+export default ToDoApp;
